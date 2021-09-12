7e5158a24371eb63ffae5ac2194937c07224daf4
Chloe Morgan <52977984+chloeanalyst@users.noreply.github.com>
2021-09-12 01:34:54 +0100

    Multiple Distances
    Added functionality within the script to calculate multiple distances in 1 call.

--- Script.R
+++ Script.R
@@ -2,6 +2,7 @@
 library(dplyr)
 library(readr)
 library(geosphere)
+library(purrr)
 
 #------------------------------------------------------------------------------- LOAD REQUIRED FUNCTIONS
 source("Functions.R")
@@ -24,12 +25,33 @@ hotel <- hotel %>% select(Longitude,Latitude)                                   
 #------------------------------------------------------------------------------- CALCULATE COVERAGE
 
 
-output <- get.multi.coverage(hotel,stop_data,5)                                  # Call Function
-output <- output %>% cbind(stop_locations)                                       # Add point names and lat longs to coverage data
-output <- output %>% distinct                                                    # Ensures data is unique.
+
+distances <- c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,                           # Define distances
+               1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2,
+               2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,3
+               )
+
+
+walking_time <- function(i) {                                                   # Function for distance mapping
+  
+  distance <- i
+  print(i)
+  output <- get.multi.coverage(hotel,stop_data,i)                                  
+  output <- output %>% cbind(stop_locations)                                       
+  output <- output %>% distinct 
+  output$distance <- distance
+  return(output)
+
+}
+
+
+result <- map_df(distances, walking_time)                                       # map distances to coverage results.
+
+
+write.csv(result, "NewYork_Coverage.csv", row.names = F)                        # Save results as CSV 
 
 
 
 
-
+result %>% filter(distance == 1) %>% arrange(desc(`Coverage %`))
 
